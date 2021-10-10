03d14d17012fa52ad04ae8813b72e3b1de0b1ef8
Babar Baig <BobBaig@outlook.com>
2021-10-10 04:23:44 -0400

    Debugging code

--- walk.js
+++ walk.js
@@ -39,21 +39,30 @@ const area = {
     // TODO: Set newBall properties: dx, dy, width, height
     newBall.dx = dx
     newBall.dy = dy
-    newBall.width = "20px"
-    newBall.height = "20px"
+    newBall.width = 20
+    newBall.height = 20
   
     // TODO: set the newBall.element property and initialize it to a Html element "div"
+    newBall.element = document.createElement("div")
   
     // TODO: set the backgroundColor, width and height style properties for newBall.element
+    newBall.element.style.backgroundColor = color
+    // newBall.element.style.left = dx + "px"
+    // newBall.element.style.top = dy + "px"
+    newBall.element.style.width = newBall.width + "px"
+    newBall.element.style.height = newBall.height + "px"
   
     // This line set the CSS class for newBall.element. No need to change this line
-    newBall.element.className += ' ball';
+    newBall.element.className += 'ball';
   
     // TODO: set the width and height of newBall based on newBall.element
     // Hint: use the Javascript parseInt() function to convert a string value to integer
+    newBall.width = parseInt(newBall.element.style.width)
+    newBall.height= parseInt(newBall.element.style.height)
   
     // TODO: use the Javascript appendChild() function to add newBall.element to the area element
-  
+    area.element.appendChild(newBall.element)
+
     return newBall;
   }
   
@@ -73,6 +82,9 @@ const area = {
   moveTo(ball1, 1, 1);
   moveTo(ball2, 10, 10);
   moveTo(ball3, 20, 20);
+
+  var elm = document.getElementsByClassName("ball");
+  console.log(elm.length)
   
   // Uncomment these lines for step 2 of the activity
   // This is expected to make the 3 balls move around the area div
